950a0807a2292c15844f999d3381a0a1675085c2
mavega998 <mavega998@misena.edu.co>
2016-05-09 14:49:58 -0500

    Subiendo archivos del taller

--- js/contador.js
+++ js/contador.js
@@ -1,9 +1,9 @@
 var contador = 0;
-var fin_contador = 70; // Tiempo en en el que deseas que redireccione la funcion.
+var fin_contador = 15; // Tiempo en en el que deseas que redireccione la funcion.
 var iniciado = false;
 function cuenta(){
 	if(contador >= fin_contador){
-		window.location.href = "taller1.html";
+		window.location.href = "home.html";
 	}else{
 		document.getElementById("contador").innerHTML  = "Comenzamos en " + fin_contador + " seg";
 		fin_contador = fin_contador - 1;
