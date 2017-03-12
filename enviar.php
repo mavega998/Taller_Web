cd100ee39184b95aabed68c171bad6f059982b81
mavega998 <mavega998@misena.edu.co>
2017-03-12 18:24:40 -0500

    agregando seccion de contacto

---
+++ enviar.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nombre = $_POST['name'];
+	$correo = $_POST['email'];
+	$mensaje = $_POST['comment'];
+    $headers =  'MIME-Version: 1.0' . "\r\n"; 
+    $headers .= 'From: Your name <info@address.com>' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+    if(!empty($nombre) && !empty($correo) && !empty($mensaje) && isset($nombre) && isset($correo) && isset($mensaje)){
+        mail ("mavega998@misena.edu.co","Mensaje de Taller Web",$nombre."\n".$correo."\n".$mensaje,$headers);
+        echo "Sus datos se recibieron correctamente...";
+    } else {
+        echo "Hubo un error en la recepcion de datos...";
+    }
+
+?>
--- js/contador.js
+++ js/contador.js
@@ -1,5 +1,5 @@
 var contador = 0;
-var fin_contador = 15; // Tiempo en en el que deseas que redireccione la funcion.
+var fin_contador = 120; // Tiempo en en el que deseas que redireccione la funcion.
 var iniciado = false;
 function cuenta(){
 	if(contador >= fin_contador){
